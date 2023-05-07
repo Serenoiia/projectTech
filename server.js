01cb00f9587295e2588cf722957d8edee61342ae
Serenoiia <jeannet.welboren@ziggo.nl>
2023-05-07 21:47:28 +0200

    week2 changes

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hiiihiii!')
-})
+app.get('/', onhome)
+app.get('/about', onabout)
+app.listen(3000)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+function onhome (req, res) {
+  res.send('<h1>Hiiihiii!</h1>')
+}
+
+function onabout (req, res) {
+  res.send('<h2>Suffer :)</h2>')
+}
+
+
+// Voorbeeld van static mapje inladen
+// app.use(express.static('naam mapje'))
+
+
+// app.get('/',(req, res) => {
+//   res.send('<h1>voorbeeld van de arrow function</h1>')
+// });
+
+
+// Les 1 manier
+// app.get('/', (req, res) => {
+//   res.send('Hiiihiii!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
