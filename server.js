48c4641869b61b4336a829eee68fe6477d48ff1d
Serenoiia <jeannet.welboren@ziggo.nl>
2023-05-08 13:09:58 +0200

    Back-end week 2

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get('/', onhome)
 app.get('/about', onabout)
 app.listen(3000)
 
+app.use(express.static('assets'))
+
+// 404 error if page is not found
+app.use((req, res, next) => {
+  res.status(404).send(
+      "<h1>404 Page not found on the server</h1>")
+})
+
 function onhome (req, res) {
   res.send('<h1>Hiiihiii!</h1>')
 }
