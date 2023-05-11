3c9a1eba6ff95a3b19d1a1505b6f1cae164395d6
Serenoiia <jeannet.welboren@ziggo.nl>
2023-05-11 14:12:41 +0200

    Back-end week 3 changes

--- server.js
+++ server.js
@@ -3,33 +3,35 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.get('/', onhome)
-app.get('/about', onabout)
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(express.static('assets'))
 app.listen(3000)
 
-app.set('view engine', 'ejs')
-app.use(express.static('assets'))
-
+// Home
+app.get('/',(req, res) => {
+  res.send('<h1>Hiiihiii!</h1>')
+})
+// Make new profile page
+app.get('/newProfile',(req, res) => {
+  res.render('make-profile.ejs');
+})
+// Select the band/artist page
+app.post('/select-artists',(req, res) => {
+  res.render('select-artists.ejs');
+})
+// Upload profile picture page
+app.get('/upload-picture',(req, res) => {
+  res.render('upload-picture.ejs');
+})
+// Fill in about info
+app.get('/newAbout',(req, res) => {
+  res.render('make-about.ejs');
+})
 // 404 error if page is not found
 app.use((req, res, next) => {
   res.status(404).send(
       "<h1>404 Page not found on the server</h1>")
 })
 
-function onhome (req, res) {
-  res.send('<h1>Hiiihiii!</h1>')
-}
 
-function onabout (req, res) {
-  res.send('<h2>Suffer :)</h2>')
-}
-
-
-
-// Voorbeeld van static mapje inladen
-// app.use(express.static('naam mapje'))
-
-
-// app.get('/',(req, res) => {
-//   res.send('<h1>voorbeeld van de arrow function</h1>')
-// });
