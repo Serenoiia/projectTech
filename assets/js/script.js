fcc700c3da6fedb9ab6fcee2885c6af75e1049e4
Serenoiia <jeannet.welboren@ziggo.nl>
2023-05-29 13:59:30 +0200

    FileReader API - error statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
   /* Image upload preview */
   const fileInput = document.querySelector("#upload-image");
+  const errorMessage = document.querySelector("#error-message");
+
   fileInput.addEventListener("change", () => {
     const file = fileInput.files[0];
     if (file) {
+      const reader = new FileReader();
+
+      reader.addEventListener("error", (event) => {
+        const error = reader.error;
+        errorMessage.textContent = `Error reading the file ${error.message}`;
+      });
+
+      reader.readAsDataURL(file);
       document.querySelector("img").src = URL.createObjectURL(file);
+      
     }
   });
 
+  
   /* Spotify embed */
   window.onSpotifyIframeApiReady = (IFrameAPI) => {
     const element = document.getElementById('embed-iframe');
